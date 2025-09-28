bdd1240d885542705b1b1b8f71dc77f9e2199bef
05126619z <05126619z@gmail.com>
2025-09-29 01:24:31 +0300

    All starts now, but crashes when try going to mods, and strafing is broken

--- src/client/java/net/nerdorg/minehop/render/ModRenderLayer.java
+++ src/client/java/net/nerdorg/minehop/render/ModRenderLayer.java
@@ -1,22 +1,28 @@
 package net.nerdorg.minehop.render;
 
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import com.mojang.blaze3d.platform.PolygonMode;
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.RenderPhase;
 import com.mojang.blaze3d.vertex.VertexFormat;
-import net.minecraft.client.render.VertexFormats;
 
 import java.util.OptionalDouble;
 
 public abstract class ModRenderLayer extends RenderLayer {
+
     public ModRenderLayer(String pName, int pBufferSize, boolean pAffectsCrumbling, boolean pSortOnUpload, Runnable pSetupState, Runnable pClearState) {
         super(pName, pBufferSize, pAffectsCrumbling, pSortOnUpload, pSetupState, pClearState);
     }
 
     public static RenderLayer getLineOfWidth(int width) {
-        return of(width + "wide_line",
-                VertexFormats.POSITION,
-                VertexFormat.DrawMode.LINES,
-                1536,
+        return of(width + "wide_line", 1536, false, false,
+                RenderPipeline.builder().withVertexFormat(VertexFormat.builder().build(), VertexFormat.DrawMode.LINES)
+                        .withCull(false)
+                        .withoutBlend()
+                        .withPolygonMode(PolygonMode.FILL)
+                        .build(),
+
+
+
                 RenderLayer.MultiPhaseParameters.builder()
                         .lineWidth(new LineWidth(OptionalDouble.of(width)))
                         .layering(VIEW_OFFSET_Z_LAYERING)
